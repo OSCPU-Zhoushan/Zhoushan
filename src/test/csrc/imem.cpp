3f6df51037a39d0eed1c5c3217e4cff33fcc219f
Li Shi <shili2048@gmail.com>
2021-08-15 13:57:30 +0800

    Fix: pc << 2 to get instructions in imem

--- src/test/csrc/imem.cpp
+++ src/test/csrc/imem.cpp
@@ -4,12 +4,13 @@ IMem::IMem()  {
   imem[0] = 0x06400413;   // addi s0, x0, 100
   imem[1] = 0x0c840493;   // addi s1, s0, 200
   imem[2] = 0x00940433;   // add  s0, s0, s1
-  imem[4] = 0x06400413;   // addi s0, x0, 100
-  imem[5] = 0x0c840493;   // addi s1, s0, 200
-  imem[6] = 0x00940433;   // add  s0, s0, s1
+  imem[3] = 0x06400413;   // addi s0, x0, 100
+  imem[4] = 0x0c840493;   // addi s1, s0, 200
+  imem[5] = 0x00940433;   // add  s0, s0, s1
 }
 
 uint32_t IMem::GetInst(uint32_t pc) {
+  pc = pc >> 2;
   if (pc > IMEM_SZ)
     return 0;
   return imem[pc];
