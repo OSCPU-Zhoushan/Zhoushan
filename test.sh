0f30351d5a7429cc3216f90d6bb58e207a9e35bc
Li Shi <shili2048@gmail.com>
2021-08-17 15:45:56 +0800

    Update: pass riscv-tests

--- test.sh
+++ test.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-examples=`cat test_list.txt`
-#examples=(select-sort)
+cpu_tests=`cat cpu_tests.txt`
+# riscv_tests=`cat riscv_tests.txt`
 
-for example in ${examples[@]}
+for example in ${cpu_tests[@]}
 do
   echo ======================================= ${example} start
   ./build/emu -i ../am-kernels/tests/cpu-tests/build/${example}-riscv64-mycpu.bin
   echo ======================================= ${example} finish
 done
+
+# for example in ${riscv_tests[@]}
+# do
+#   echo ======================================= ${example} start
+#   ./build/emu -i ../riscv-tests//build/${example}-riscv64-mycpu.bin
+#   echo ======================================= ${example} finish
+# done
 wait
