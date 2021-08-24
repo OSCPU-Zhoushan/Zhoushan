6b193af6245afdbdb58fed593500601c81e8edd6
Li Shi <shili2048@gmail.com>
2021-08-24 10:12:44 +0800

    Update: auto test script

--- test.sh
+++ test.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-cpu_tests=`cat cpu_tests.txt`
-# riscv_tests=`cat riscv_tests.txt`
+function cpu_tests {
+  cpu_tests=`cat cpu_tests.txt`
+  for example in ${cpu_tests[@]}
+  do
+    echo ======================================= ${example} start
+    ./build/emu -i ../am-kernels/tests/cpu-tests/build/${example}-riscv64-mycpu.bin
+    echo ======================================= ${example} finish
+  done
+  wait
+}
 
-for example in ${cpu_tests[@]}
-do
-  echo ======================================= ${example} start
-  ./build/emu -i ../am-kernels/tests/cpu-tests/build/${example}-riscv64-mycpu.bin
-  echo ======================================= ${example} finish
+function riscv_tests {
+  riscv_tests=`cat riscv_tests.txt`
+  for example in ${riscv_tests[@]}
+  do
+    echo ======================================= ${example} start
+    ./build/emu -i ../riscv-tests//build/${example}-riscv64-mycpu.bin
+    echo ======================================= ${example} finish
+  done
+  wait
+}
+
+
+while getopts 'crt:' OPT; do
+  case $OPT in
+    c)
+      cpu_tests;;
+    r)
+      riscv_tests;;
+    t)
+      example="$OPTARG"
+      ./build/emu -i ../am-kernels/tests/cpu-tests/build/${example}-riscv64-mycpu.bin -b 0 -e 1000 --dump-wave;;
+    ?)
+      echo "Error: missing arguments"
+  esac
 done
-
-# for example in ${riscv_tests[@]}
-# do
-#   echo ======================================= ${example} start
-#   ./build/emu -i ../riscv-tests//build/${example}-riscv64-mycpu.bin
-#   echo ======================================= ${example} finish
-# done
-wait
