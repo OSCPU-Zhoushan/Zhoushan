59e59ecd544721922086d39b045c067bdaf0cece
Li Shi <shili2048@gmail.com>
2021-08-24 16:46:37 +0800

    add Clint file(empty) and add bitutils from Nutshell to RegMap file

--- src/main/scala/RegMap.scala
+++ src/main/scala/RegMap.scala
@@ -24,3 +24,33 @@ object RegMap {
     access(mapping, addr, rdata, ren, addr, wdata, wen)
   }
 }
+
+//ref from BitUtils.scala
+object WordShift {
+  def apply(data: UInt, wordIndex: UInt, step: Int) = (data << (wordIndex * step.U))
+}
+
+object MaskExpand {
+ def apply(m: UInt) = Cat(m.asBools.map(Fill(8, _)).reverse)
+}
+
+object MaskData {
+ def apply(oldData: UInt, newData: UInt, fullmask: UInt) = {
+   (newData & fullmask) | (oldData & ~fullmask)
+ }
+}
+
+object SignExt {
+  def apply(a: UInt, len: Int) = {
+    val aLen = a.getWidth
+    val signBit = a(aLen-1)
+    if (aLen >= len) a(len-1,0) else Cat(Fill(len - aLen, signBit), a)
+  }
+}
+
+object ZeroExt {
+  def apply(a: UInt, len: Int) = {
+    val aLen = a.getWidth
+    if (aLen >= len) a(len-1,0) else Cat(0.U((len - aLen).W), a)
+  }
+}
