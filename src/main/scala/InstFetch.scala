9097aeaeabd55b5b398d9875a6f1265aeed71826
Li Shi <shili2048@gmail.com>
2021-09-04 14:41:51 +0800

    Update: IF structure

--- src/main/scala/InstFetch.scala
+++ src/main/scala/InstFetch.scala
@@ -24,82 +24,93 @@ abstract class InstFetchModule extends Module {
 
 class InstFetch extends InstFetchModule {
   val io = IO(new InstFetchIO)
+  val stall = io.stall
 
   val req = io.imem.req
   val resp = io.imem.resp
 
-  val stall = io.stall
-  val reg_stall = RegNext(stall)
-
   val pc_init = "h80000000".U(32.W)
-  val pc_next = RegInit(pc_init)
-  val pc = RegInit(pc_init)
-  val pc_valid = RegInit(false.B)
-
-  val inst = WireInit(UInt(32.W), 0.U)
-  val inst_valid = WireInit(false.B)
 
   val bp = Module(new BrPredictor)
-  val bp_pred_pc = bp.io.pred_pc
-  val pred_br = RegInit(false.B)
-  val pred_pc = RegInit(UInt(32.W), 0.U)
+  bp.io.jmp_packet <> io.jmp_packet
 
-  val mis = io.jmp_packet.mis
-  val mis_pc = Mux(io.jmp_packet.jmp, io.jmp_packet.jmp_pc, io.jmp_packet.inst_pc + 4.U)
-  val reg_mis = RegInit(false.B)
-  val reg_mis_pc = RegInit(UInt(32.W), 0.U)
-  when (io.jmp_packet.mis && !resp.fire()) {
-    reg_mis := true.B
-    reg_mis_pc := mis_pc
+  /* ----- IF Stage 1 ---------------- */
+
+  val s1_pc = RegInit(pc_init)
+  val s1_pred_br = bp.io.pred_br
+  val s1_pred_pc = bp.io.pred_pc
+  bp.io.pc := s1_pc
+
+  /* ----- IF Stage 2 ---------------- */
+
+  val s2_pc = RegInit(pc_init)
+  val s2_pc_valid = RegInit(false.B)
+
+  val s2_pred_br = RegInit(false.B)
+  val s2_pred_pc = RegInit(UInt(32.W), 0.U)
+  val s2_inst = WireInit(UInt(32.W), 0.U)
+  val s2_inst_valid = WireInit(false.B)
+
+  val s1_mis = io.jmp_packet.mis
+  val s1_mis_pc = Mux(io.jmp_packet.jmp, io.jmp_packet.jmp_pc, io.jmp_packet.inst_pc + 4.U)
+  val s2_mis = RegInit(false.B)
+  val s2_mis_pc = RegInit(UInt(32.W), 0.U)
+
+  /* ----- Pipeline Ctrl Signals ----- */
+
+  val pipeline_valid = WireInit(false.B)
+  val pipeline_ready = WireInit(false.B)
+  val pipeline_fire  = pipeline_valid && pipeline_ready
+
+  when (s1_mis && !resp.fire()) {
+    s2_mis := true.B
+    s2_mis_pc := s1_mis_pc
   }
 
-  req.bits.addr := pc_next
-  req.bits.ren := true.B          // read-only imem
+  // handshake signals with I cache
+
+  req.bits.addr := s1_pc
+  req.bits.ren := true.B
   req.bits.wdata := 0.U
   req.bits.wmask := 0.U
   req.bits.wen := false.B
-  req.valid := !stall && !io.jmp_packet.mis
-  
-  resp.ready := !stall
-  
-  /* Branch predictor logic */
+  req.valid := !stall && !s1_mis
 
-  bp.io.pc := pc_next
-  bp.io.jmp_packet <> io.jmp_packet
+  resp.ready := !stall
 
   // update pc when req.fire()
 
-  when (req.fire() || io.jmp_packet.mis) {
-    pc := pc_next
-    pred_br := bp.io.pred_br
-    pred_pc := bp.io.pred_pc
-    pc_valid := true.B
-    when (io.jmp_packet.mis) {
-      pc_next := mis_pc
-      pc_valid := false.B
-    } .elsewhen (reg_mis) {
-      pc_next := reg_mis_pc
-      pc_valid := false.B
-      reg_mis := false.B
+  when (req.fire() || s1_mis) {
+    s2_pc := s1_pc
+    s2_pred_br := s1_pred_br
+    s2_pred_pc := s1_pred_pc
+    s2_pc_valid := true.B
+    when (s1_mis) {
+      s1_pc := s1_mis_pc
+      s2_pc_valid := false.B
+    } .elsewhen (s2_mis) {
+      s1_pc := s2_mis_pc
+      s2_pc_valid := false.B
+      s2_mis := false.B
     } .otherwise {
-      pc_next := bp_pred_pc
+      s1_pc := s1_pred_pc
     }
   }
 
-  // update inst when 
+  // update inst when resp.fire()
 
-  inst_valid := false.B
+  s2_inst_valid := false.B
   when (resp.fire()) {
-    inst := Mux(pc(2), resp.bits.rdata(63, 32), resp.bits.rdata(31, 0))
-    inst_valid := !reg_mis && pc_valid
+    s2_inst := Mux(s2_pc(2), resp.bits.rdata(63, 32), resp.bits.rdata(31, 0))
+    s2_inst_valid := !s2_mis && s2_pc_valid
   }
 
-  io.out.pc := pc
-  io.out.inst := inst
-  io.out.valid := inst_valid
+  io.out.pc := s2_pc
+  io.out.inst := s2_inst
+  io.out.valid := s2_inst_valid
 
-  io.out.pred_br := pred_br
-  io.out.pred_pc := pred_pc
+  io.out.pred_br := s2_pred_br
+  io.out.pred_pc := s2_pred_pc
 
 }
 
