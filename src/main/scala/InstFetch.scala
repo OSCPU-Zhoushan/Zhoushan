6f55724f3a5916a08ef4c8222b5ce7570def0262
Li Shi <shili2048@gmail.com>
2021-09-04 20:57:45 +0800

    Update: IF 2 stage pipeline

--- src/main/scala/InstFetch.scala
+++ src/main/scala/InstFetch.scala
@@ -37,8 +37,10 @@ class InstFetch extends InstFetchModule {
   /* ----- IF Stage 1 ---------------- */
 
   val s1_pc = RegInit(pc_init)
-  val s1_pred_br = bp.io.pred_br
-  val s1_pred_pc = bp.io.pred_pc
+
+  val s1_mis = io.jmp_packet.mis
+  val s1_mis_pc = Mux(io.jmp_packet.jmp, io.jmp_packet.jmp_pc, io.jmp_packet.inst_pc + 4.U)
+
   bp.io.pc := s1_pc
 
   /* ----- IF Stage 2 ---------------- */
@@ -46,25 +48,44 @@ class InstFetch extends InstFetchModule {
   val s2_pc = RegInit(pc_init)
   val s2_pc_valid = RegInit(false.B)
 
-  val s2_pred_br = RegInit(false.B)
-  val s2_pred_pc = RegInit(UInt(32.W), 0.U)
+  val s2_pred_br = WireInit(false.B)
+  val s2_pred_pc = WireInit(UInt(32.W), 0.U)
+  val s2_reg_pred_br = RegInit(false.B)
+  val s2_reg_pred_pc = RegInit(UInt(32.W), 0.U)
+  val s2_reg_pred_valid = RegInit(false.B)
   val s2_inst = WireInit(UInt(32.W), 0.U)
   val s2_inst_valid = WireInit(false.B)
 
-  val s1_mis = io.jmp_packet.mis
-  val s1_mis_pc = Mux(io.jmp_packet.jmp, io.jmp_packet.jmp_pc, io.jmp_packet.inst_pc + 4.U)
-  val s2_mis = RegInit(false.B)
-  val s2_mis_pc = RegInit(UInt(32.W), 0.U)
-
   /* ----- Pipeline Ctrl Signals ----- */
 
-  val pipeline_valid = WireInit(false.B)
-  val pipeline_ready = WireInit(false.B)
-  val pipeline_fire  = pipeline_valid && pipeline_ready
+  val init = RegInit(true.B)
+  when (req.valid) {
+    init := false.B
+  }
 
-  when (s1_mis && !resp.fire()) {
-    s2_mis := true.B
-    s2_mis_pc := s1_mis_pc
+  val pipeline_valid = req.fire()
+  val pipeline_ready = resp.fire() || init
+  val pipeline_fire  = pipeline_valid && pipeline_ready
+  val pipeline_clear = s1_mis
+
+  when (pipeline_clear) {
+    s1_pc := s1_mis_pc
+  } .elsewhen (pipeline_fire) {
+    s1_pc := s1_pc + 4.U
+  }
+
+  when(pipeline_clear) {
+    s2_pc_valid := false.B
+  } .elsewhen (pipeline_fire) {
+    s2_pc := s1_pc
+    s2_pc_valid := !s1_mis
+    s2_pred_br := bp.io.pred_br
+    s2_pred_pc := bp.io.pred_pc
+    s2_reg_pred_valid := false.B
+  } .elsewhen (!pipeline_fire && RegNext(pipeline_fire)) {
+    s2_reg_pred_br := s2_pred_br
+    s2_reg_pred_pc := s2_pred_pc
+    s2_reg_pred_valid := true.B
   }
 
   // handshake signals with I cache
@@ -78,39 +99,20 @@ class InstFetch extends InstFetchModule {
 
   resp.ready := !stall
 
-  // update pc when req.fire()
-
-  when (req.fire() || s1_mis) {
-    s2_pc := s1_pc
-    s2_pred_br := s1_pred_br
-    s2_pred_pc := s1_pred_pc
-    s2_pc_valid := true.B
-    when (s1_mis) {
-      s1_pc := s1_mis_pc
-      s2_pc_valid := false.B
-    } .elsewhen (s2_mis) {
-      s1_pc := s2_mis_pc
-      s2_pc_valid := false.B
-      s2_mis := false.B
-    } .otherwise {
-      s1_pc := s1_pred_pc
-    }
-  }
-
   // update inst when resp.fire()
 
   s2_inst_valid := false.B
-  when (resp.fire()) {
+  when (pipeline_fire) {
     s2_inst := Mux(s2_pc(2), resp.bits.rdata(63, 32), resp.bits.rdata(31, 0))
-    s2_inst_valid := !s2_mis && s2_pc_valid
+    s2_inst_valid := !pipeline_clear && s2_pc_valid
   }
 
   io.out.pc := s2_pc
   io.out.inst := s2_inst
   io.out.valid := s2_inst_valid
 
-  io.out.pred_br := s2_pred_br
-  io.out.pred_pc := s2_pred_pc
+  io.out.pred_br := Mux(s2_reg_pred_valid, s2_reg_pred_br, s2_pred_br)
+  io.out.pred_pc := Mux(s2_reg_pred_valid, s2_reg_pred_pc, s2_pred_pc)
 
 }
 
