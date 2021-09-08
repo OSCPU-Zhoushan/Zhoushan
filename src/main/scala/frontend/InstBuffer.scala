7ccff5264d54530a159331c756c22120f176aded
Li Shi <shili2048@gmail.com>
2021-09-08 15:39:39 +0800

    Fix: name

--- src/main/scala/frontend/InstBuffer.scala
+++ src/main/scala/frontend/InstBuffer.scala
@@ -11,8 +11,8 @@ class InstPacket extends Bundle {
 }
 
 class InstPacketVec extends Bundle with ZhoushanConfig {
-  val enq_width = FetchWidth
-  val vec = Vec(enq_width, Valid(new InstPacket))
+  val width = FetchWidth
+  val vec = Vec(width, Valid(new InstPacket))
 }
 
 class InstBuffer extends Module with ZhoushanConfig {
