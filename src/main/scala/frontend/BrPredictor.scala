25d5d2da2dbbe3f0e22ebe36ff4226d8b1be0038
Li Shi <shili2048@gmail.com>
2021-09-10 13:31:37 +0800

    Fix: sync write bug

--- src/main/scala/frontend/BrPredictor.scala
+++ src/main/scala/frontend/BrPredictor.scala
@@ -32,11 +32,16 @@ class PatternHistoryTable extends Module with BpParameters with ZhoushanConfig {
     pht
   }
 
+  // read from pht
   for (i <- 0 until FetchWidth) {
     val pht_rdata = WireInit(VecInit(Seq.fill(PhtWidth)(0.U(2.W))))
+
+    // stage 1
     for (j <- 0 until PhtWidth) {
       pht_rdata(j) := pht(j).read(io.raddr(i))
     }
+
+    // stage 2
     io.rdirect(i) := false.B
     for (j <- 0 until PhtWidth) {
       when (RegNext(io.rindex(i)) === j.U) {
@@ -45,16 +50,27 @@ class PatternHistoryTable extends Module with BpParameters with ZhoushanConfig {
     }
   }
 
-  val pht_wstate_r = WireInit(UInt(2.W), 0.U)   // first read PHT state
-  val pht_wstate_w = WireInit(UInt(2.W), 0.U)   // then write PHT state
-  when (io.wen) {
+  // write to pht
+  val pht_wdata = WireInit(VecInit(Seq.fill(PhtWidth)(0.U(2.W))))
+  val pht_wdata_r = WireInit(UInt(2.W), 0.U)  // first read PHT state
+  val pht_wdata_w = WireInit(UInt(2.W), 0.U)  // then write PHT state
+
+  // stage 1
+  when (RegNext(io.wen)) {
     for (j <- 0 until PhtWidth) {
-      when (io.windex === j.U) {
-        pht_wstate_r := pht(j).read(io.waddr)
+      pht_wdata(j) := pht(j).read(io.waddr)
+    }
+  }
+  
+  // stage 2
+  when (RegNext(io.wen)) {
+    for (j <- 0 until PhtWidth) {
+      when (RegNext(io.windex) === j.U) {
+        pht_wdata_r := pht_wdata(j)
       }
     }
   }
-  pht_wstate_w := MuxLookup(pht_wstate_r, 0.U, Array(
+  pht_wdata_w := MuxLookup(pht_wdata_r, 0.U, Array(
     0.U -> Mux(RegNext(io.wjmp), 1.U, 0.U),   // strongly not taken
     1.U -> Mux(RegNext(io.wjmp), 2.U, 0.U),   // weakly not taken
     2.U -> Mux(RegNext(io.wjmp), 3.U, 1.U),   // weakly taken
@@ -63,7 +79,7 @@ class PatternHistoryTable extends Module with BpParameters with ZhoushanConfig {
   when (RegNext(io.wen)) {
     for (j <- 0 until PhtWidth) {
       when (RegNext(io.windex === j.U)) {
-        pht(j).write(RegNext(io.waddr), pht_wstate_w)
+        pht(j).write(RegNext(io.waddr), pht_wdata_w)
       }
     }
   }
