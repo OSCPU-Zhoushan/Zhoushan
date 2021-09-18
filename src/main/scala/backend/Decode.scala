042b7a9a6af7629d33d3ca642ec01256df022fc9
Li Shi <shili2048@gmail.com>
2021-09-18 22:43:34 +0800

    Fix: pipeline signals

--- src/main/scala/backend/Decode.scala
+++ src/main/scala/backend/Decode.scala
@@ -18,7 +18,7 @@ class Decode extends Module with ZhoushanConfig {
 
   when (io.flush || io.out.fire()) {
     reg_in_valid := false.B
-  } .elsewhen (io.in.valid && !io.flush && !io.out.ready && RegNext(io.out.ready)) {
+  } .elsewhen (io.in.valid && !io.flush && !io.out.ready) {
     reg_in := io.in.bits.vec
     reg_in_valid := true.B
   }
