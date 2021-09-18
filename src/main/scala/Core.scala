499575e7b6719d6d6076c093e2086d7ce4bdff1d
Li Shi <shili2048@gmail.com>
2021-09-18 21:26:04 +0800

    Update: debug msg for uop commit

--- src/main/scala/Core.scala
+++ src/main/scala/Core.scala
@@ -135,8 +135,8 @@ class Core extends Module with ZhoushanConfig {
       if (DebugMsgCommit) {
         val u = cm(i)
         when (u.valid) {
-          printf("%d: [CM %d ] pc=%x inst=%x rs1=%d->%d rs2=%d->%d rd(en=%x)=%d->%d\n", DebugTimer(), i.U, 
-                 u.pc, u.inst, u.rs1_addr, u.rs1_paddr, u.rs2_addr, u.rs2_paddr, u.rd_en, u.rd_addr, u.rd_paddr)
+          printf("%d: [CM %d ] pc=%x inst=%x rs1=%d->%d rs2=%d->%d rd(en=%x)=%d->%d bp(br=%x bpc=%x) rob=%d\n", DebugTimer(), i.U, 
+                 u.pc, u.inst, u.rs1_addr, u.rs1_paddr, u.rs2_addr, u.rs2_paddr, u.rd_en, u.rd_addr, u.rd_paddr, u.pred_br, u.pred_bpc, u.rob_addr)
         }
       }
     }
