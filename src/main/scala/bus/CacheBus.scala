8b621f9436068354d7b029c54c6e0c6ffa5d76a7
Li Shi <shili2048@gmail.com>
2021-09-21 17:43:49 +0800

    Update: crossbar2to1 for cachebus

--- src/main/scala/bus/CacheBus.scala
+++ src/main/scala/bus/CacheBus.scala
@@ -7,7 +7,11 @@ trait CacheBusParameters {
   val CacheBusUserWidth = 64 + ZhoushanConfig.FetchWidth * 2
 }
 
-class CacheBusReq extends Bundle with AxiParameters with CacheBusParameters {
+trait CacheBusId extends Bundle with AxiParameters {
+  val id = Output(UInt(AxiIdWidth.W))
+}
+
+class CacheBusReq extends Bundle with AxiParameters with CacheBusId with CacheBusParameters {
   val addr = Output(UInt(AxiAddrWidth.W))
   val ren = Output(Bool())
   val wdata = Output(UInt(AxiDataWidth.W))
@@ -16,7 +20,7 @@ class CacheBusReq extends Bundle with AxiParameters with CacheBusParameters {
   val user = Output(UInt(CacheBusUserWidth.W))
 }
 
-class CacheBusResp extends Bundle with AxiParameters with CacheBusParameters {
+class CacheBusResp extends Bundle with AxiParameters with CacheBusId with CacheBusParameters {
   val rdata = Output(UInt(AxiDataWidth.W))
   val user = Output(UInt(CacheBusUserWidth.W))
 }
