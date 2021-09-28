875a7348bd61b43bacd5eb68c5da3963547977c6
Li Shi <shili2048@gmail.com>
2021-09-28 21:28:48 +0800

    CacheBus: code reformat

--- src/main/scala/bus/CacheBus.scala
+++ src/main/scala/bus/CacheBus.scala
@@ -11,18 +11,20 @@ trait CacheBusId extends Bundle with AxiParameters {
   val id = Output(UInt(AxiIdWidth.W))
 }
 
-class CacheBusReq extends Bundle with AxiParameters with CacheBusId with CacheBusParameters {
+trait CacheBusUser extends Bundle with CacheBusParameters {
+  val user = Output(UInt(CacheBusUserWidth.W))
+}
+
+class CacheBusReq extends Bundle with CacheBusId with CacheBusUser {
   val addr = Output(UInt(AxiAddrWidth.W))
   val ren = Output(Bool())
   val wdata = Output(UInt(AxiDataWidth.W))
   val wmask = Output(UInt((AxiDataWidth / 8).W))
   val wen = Output(Bool())
-  val user = Output(UInt(CacheBusUserWidth.W))
 }
 
-class CacheBusResp extends Bundle with AxiParameters with CacheBusId with CacheBusParameters {
+class CacheBusResp extends Bundle with CacheBusId with CacheBusUser {
   val rdata = Output(UInt(AxiDataWidth.W))
-  val user = Output(UInt(CacheBusUserWidth.W))
 }
 
 class CacheBusIO extends Bundle {
