fdb8e517fd5750f54963958e8dbe93397261924c
Li Shi <shili2048@gmail.com>
2021-10-07 16:10:39 +0800

    SoC: remove unused bits

--- src/main/scala/backend/IssueUnit.scala
+++ src/main/scala/backend/IssueUnit.scala
@@ -426,7 +426,7 @@ class IssueQueueInOrder(entries: Int, enq_width: Int, deq_width: Int) extends Ab
     }
   }
 
-  val valid_vec = Mux(count >= deq_width.U, ((1 << deq_width) - 1).U, UIntToOH(count)(deq_width - 1, 0) - 1.U)
+  val valid_vec = Mux(count >= deq_width.U, ((1 << deq_width) - 1).U, UIntToOH(count, deq_width) - 1.U)
   val next_deq_vec = VecInit(deq_vec.map(_ + num_deq))
   deq_vec := next_deq_vec
 
--- src/main/scala/frontend/InstBuffer.scala
+++ src/main/scala/frontend/InstBuffer.scala
@@ -99,7 +99,7 @@ class InstBuffer extends Module with ZhoushanConfig {
 
   // deq
 
-  val valid_vec = Mux(count >= deq_width.U, ((1 << deq_width) - 1).U, UIntToOH(count)(deq_width - 1, 0) - 1.U)
+  val valid_vec = Mux(count >= deq_width.U, ((1 << deq_width) - 1).U, UIntToOH(count, deq_width) - 1.U)
   val next_deq_vec = VecInit(deq_vec.map(_ + num_deq))
   deq_vec := next_deq_vec
 
